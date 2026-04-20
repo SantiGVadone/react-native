26b6978866f1df8759f47081d7746194985c758e
SantiGVadone <santiagovadone36@gmail.com>
2026-04-19 22:18:34 -0300

    Install react-navigation for screen-travel

--- my-expo-app/App.tsx
+++ my-expo-app/App.tsx
@@ -1,12 +1,25 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import './global.css';
+import { createStaticNavigation } from '@react-navigation/native';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: {
+      screen: Home,
+      options: { title: 'Welcome' },
+    },
+  },
+});
+
+const Navigate = createStaticNavigation(RootStack);
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent></ScreenContent>
+      <Navigate></Navigate>
       <StatusBar style="light" />
     </SafeAreaProvider>
   );
--- my-expo-app/components/ScreenContent.tsx
+++ my-expo-app/components/ScreenContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { HomePage } from './HomePage';
+import { HomePage } from './Home';
 import { Navbar } from './Navbar';
 
 export const ScreenContent: React.FC = () => {
