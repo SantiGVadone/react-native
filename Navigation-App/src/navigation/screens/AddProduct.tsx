a43b7acbb8d855552e10432b1b0a9b15cb10e8a6
SantiGVadone <santiagovadone36@gmail.com>
2026-04-22 22:34:39 -0300

    Initial commit for AddProduct Screen

--- Navigation-App/src/navigation/screens/AddProduct.tsx
+++ Navigation-App/src/navigation/screens/AddProduct.tsx
@@ -1,9 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useState } from 'react'
+
+interface Product {
+  id: string
+  nombre: string
+  descripcion: string
+  cantidad: number
+}
 
 export function AddProduct() {
+  const [product, setProduct] = useState<Product>({
+    id: '',
+    nombre: '',
+    descripcion: '',
+    cantidad: 0,
+  })
   return (
-    <View>
-      <Text>Aca quiero que se pueda Crear un Producto nuevo digamos</Text>
-    </View>
+    <SafeAreaView className='flex-1'>
+      <View className='flex-1'>
+        <View className='flex-1 items-center'>
+          <TextInput
+            placeholder='Nombre'
+            autoCorrect={false}
+            showSoftInputOnFocus={true}
+            className='h-16 m-4 border-b-2 text-center text-3xl w-3/4'
+            onChangeText={() => setProduct}
+            value={product.nombre}
+          />
+          <TextInput
+            placeholder='Descripción'
+            autoCorrect={false}
+            showSoftInputOnFocus={true}
+            className='h-16 m-4 border-b-2 text-center text-3xl w-3/4'
+            onChangeText={() => setProduct}
+            value={product.descripcion}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
