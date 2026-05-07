f30a8496571f48cfc5d7a486683d415389236e45
SantiGVadone <santiagovadone36@gmail.com>
2026-05-07 17:46:43 -0300

    delete tailwind from project

--- Navigation-App/babel.config.js
+++ Navigation-App/babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel', // Algunos sugieren quitar este si usas el de arriba, pero v4 suele pedirlo.
-    ],
-    plugins: [
-      'react-native-reanimated/plugin', // SIEMPRE ÚLTIMO
-    ],
+    presets: ['babel-preset-expo'],
   }
 }
--- Navigation-App/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.tsx', './src/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')], // ESTO ES CLAVE EN V4
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
